b8ead2cfc3d93fd132fad7f03d38a11943b7f897
Jaewan Yun <jay50@pitt.edu>
2018-02-04 19:17:13 -0800

    add results

--- optimizer_test.py
+++ optimizer_test.py
@@ -6,13 +6,12 @@ import numpy as np
 import tensorflow as tf
 
 
-def f1(x, y):
-#     z = x * x + y * y
-    z = (x * x + y * y) * (math.cos(x + y) + 1.2)
+def contour_f(x, y):
+    z = (x ** 2 + y ** 2) * (math.cos(x + y) + 1.2)
     return z
 
 
-def f2(x, y):
+def tf_f(x, y):
     z = (tf.square(x) + tf.square(y)) * (tf.cos(x + y) + 1.2)
     return z
 
@@ -21,7 +20,7 @@ plt.ion()
 
 for x in np.arange(-10.0, 10.0, 0.1):
     for y in np.arange(-10.0, 10.0, 0.1):
-        z = f1(x, y)
+        z = contour_f(x, y)
 
         if 1 < z < 1.3:
             plt.scatter(x, y, color='g', s=1)
@@ -29,24 +28,41 @@ for x in np.arange(-10.0, 10.0, 0.1):
         if 2 < z < 2.5:
             plt.scatter(x, y, color='g', s=1)
 
-        if 5 < z < 6:
+        if 5.5 < z < 6.5:
             plt.scatter(x, y, color='g', s=1)
 
 plt.draw()
 
-x = tf.Variable(-2, [1], dtype=tf.float32)
-y = tf.Variable(4, [1], dtype=tf.float32)
+x_i = -1.8
+y_i = 3.8
+
+x = tf.Variable(x_i, [1], dtype=tf.float32)
+y = tf.Variable(y_i, [1], dtype=tf.float32)
 
 with tf.Session() as sess:
-    cost = f2(x, y)
+    cost = tf_f(x, y)
 
     # change optimizers here
-    op = tf.train.AdamOptimizer(0.01).minimize(cost)
+    op = tf.train.AdamOptimizer(0.5).minimize(cost)
 
     sess.run(tf.global_variables_initializer())
 
-    for _ in range(10000):
+    last_x = x_i
+    last_y = y_i
+    last_point = None
+
+    for _ in range(50):
         _, x_val, y_val = sess.run([op, x, y])
-        plt.scatter(x_val, y_val, color='r', s=1)
+
+        if last_point:
+            last_point.remove()
+        last_point = plt.scatter(x_val, y_val, color='r', s=20)
+
+        if last_x and last_y:
+            plt.plot([last_x, x_val], [last_y, y_val], color='r')
+        last_x = x_val
+        last_y = y_val
         plt.pause(.001)
 
+print("done")
+plt.pause(100)
